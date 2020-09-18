4916d4fe373231c548a6153477029c74c056f66b
Rui <33549232+user-cube@users.noreply.github.com>
2020-09-19 00:16:06 +0100

    Setup
    Clone git repositories in use

---
+++ setup.sh
@@ -0,0 +1,11 @@
+cd .vim/bundle &&
+git clone https://github.com/skywind3000/asyncrun.vim &&
+git clone https://github.com/drewtempelmeyer/palenight.vim &&
+git clone https://github.com/tyrannicaltoucan/vim-quantum &&
+git clone https://github.com/Raimondi/delimitMate &&
+git clone https://github.com/preservim/nerdcommenter &&
+git clone https://github.com/VundleVim/Vundle.vim &&
+git clone https://github.com/initrc/eclim-vundle &&
+git clone https://github.com/preservim/nerdtree &&
+git clone https://github.com/ycm-core/YouCompleteMe &&
+git clone https://github.com/ryanoasis/vim-devicons
